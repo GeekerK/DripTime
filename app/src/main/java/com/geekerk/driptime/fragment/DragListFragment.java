e970981797a08efe4e9df11f76ab7a50438ec8c3
s21v <s21v@gmail.com>
2016-05-09 16:44:13 +0800

    添加上下拖动

--- app/src/main/java/com/geekerk/driptime/fragment/DragListFragment.java
+++ app/src/main/java/com/geekerk/driptime/fragment/DragListFragment.java
@@ -5,10 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.geekerk.driptime.adapter.DragRecyclerViewAdapter;
 
 
@@ -22,6 +22,32 @@ public class DragListFragment extends Fragment {
         RecyclerView recyclerView = new RecyclerView(getActivity());
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(new DragRecyclerViewAdapter(getActivity()));
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP|ItemTouchHelper.DOWN, 0) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                recyclerView.getAdapter().notifyItemMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+            }
+
+            @Override
+            public boolean isLongPressDragEnabled() {
+                return true;
+            }
+
+            @Override
+            public boolean canDropOver(RecyclerView recyclerView, RecyclerView.ViewHolder current, RecyclerView.ViewHolder target) {
+                if (target instanceof DragRecyclerViewAdapter.TextViewHolder)
+                    return false;
+                return super.canDropOver(recyclerView, current, target);
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerView);
         return recyclerView;
     }
 }
